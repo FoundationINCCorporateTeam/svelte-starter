dfa16b8eef66a3018f714c6e67f64c1c8da61571
FoundationINCCorporateTeam <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-02-14 16:17:47 -0600

    Update clerk.server.js

--- src/lib/server/clerk.server.js
+++ src/lib/server/clerk.server.js
@@ -3,8 +3,8 @@ import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
 import { Clerk } from '@clerk/clerk-sdk-node';
 
 const clerk = Clerk({
-	secretKey: CLERK_SECRET_KEY,
-	publishableKey: PUBLIC_CLERK_PUBLISHABLE_KEY
+	secretKey: sk_test_Gcvh76lrPtdw47vwLL8T0mj5kZmx5L15y0mLxEx082,
+	publishableKey: pk_test_dG9nZXRoZXItZ2hvdWwtNy5jbGVyay5hY2NvdW50cy5kZXYk
 });
 
 export default clerk;
